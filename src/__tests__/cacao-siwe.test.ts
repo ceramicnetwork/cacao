170563f44ae4db32eca39f0b6d8b4fd409d01bb1
Sergey Ukustov <sergey@ukstv.me>
2022-08-25 14:57:50 +0300

    chore: Reorg tests: clearer date manipulation

--- src/__tests__/cacao-siwe.test.ts
+++ src/__tests__/cacao-siwe.test.ts
@@ -1,8 +1,10 @@
 import { Wallet } from '@ethersproject/wallet'
 import { Cacao, CacaoBlock } from '../cacao.js'
 import { SiweMessage } from '../siwx/siwe.js'
+import { DateTime } from 'luxon'
 
-const ISSUED_AT = new Date('2021-10-14T07:18:41Z')
+const ISSUED_AT = DateTime.fromISO('2021-10-14T07:18:41Z').toUTC()
+const EXPIRATION_TIME = ISSUED_AT.plus({ seconds: 5 })
 
 const WALLET_MNEMONIC =
   'despair voyage estate pizza main slice acquire mesh polar short desk lyrics'
@@ -15,7 +17,7 @@ const SIWE_MESSAGE_PARAMS = {
   uri: 'did:key:z6MkrBdNdwUPnXDVD1DCxedzVVBpaGi8aSmoXFAeKNgtAer8',
   version: '1',
   nonce: '32891757',
-  issuedAt: ISSUED_AT.toISOString(),
+  issuedAt: ISSUED_AT.toISO(),
   chainId: '1',
   resources: [
     'ipfs://Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu',
@@ -46,30 +48,38 @@ test('Converts between Cacao and SiweMessage', () => {
 test('ok after exp if within phase out period', async () => {
   const msg = new SiweMessage({
     ...SIWE_MESSAGE_PARAMS,
-    expirationTime: new Date(ISSUED_AT.valueOf() + 5 * 1000).toISOString(),
+    expirationTime: EXPIRATION_TIME.toISO(),
   })
 
   msg.signature = await ETHEREUM_WALLET.signMessage(msg.toMessage())
 
   const cacao = Cacao.fromSiweMessage(msg)
-  const expiredTime = new Date(ISSUED_AT.valueOf() + 10 * 1000)
+  const expiredTime = EXPIRATION_TIME.plus({ seconds: 5 })
   expect(() =>
-    Cacao.verify(cacao, { atTime: expiredTime, revocationPhaseOutSecs: 20, clockSkewSecs: 0 })
+    Cacao.verify(cacao, {
+      atTime: expiredTime.toJSDate(),
+      revocationPhaseOutSecs: 20,
+      clockSkewSecs: 0,
+    })
   ).not.toThrow()
 })
 
 test('fail after exp if after phase out period', async () => {
   const msg = new SiweMessage({
     ...SIWE_MESSAGE_PARAMS,
-    expirationTime: new Date(ISSUED_AT.valueOf() + 5 * 1000).toISOString(),
+    expirationTime: EXPIRATION_TIME.toISO(),
   })
 
   msg.signature = await ETHEREUM_WALLET.signMessage(msg.signMessage())
 
   const cacao = Cacao.fromSiweMessage(msg)
-  const expiredTime = new Date(ISSUED_AT.valueOf() + 10 * 1000)
+  const expiredTime = EXPIRATION_TIME.plus({ seconds: 5 })
   expect(() =>
-    Cacao.verify(cacao, { atTime: expiredTime, revocationPhaseOutSecs: 1, clockSkewSecs: 0 })
+    Cacao.verify(cacao, {
+      atTime: expiredTime.toJSDate(),
+      revocationPhaseOutSecs: 1,
+      clockSkewSecs: 0,
+    })
   ).toThrow(`CACAO has expired`)
 })
 
@@ -79,14 +89,14 @@ test('ok before IAT if within default clockskew', async () => {
   msg.signature = await ETHEREUM_WALLET.signMessage(msg.toMessage())
 
   const cacao = Cacao.fromSiweMessage(msg)
-  const OneMinbeforeIAT = new Date(ISSUED_AT.valueOf() - 60 * 1000)
-  expect(() => Cacao.verify(cacao, { atTime: OneMinbeforeIAT })).not.toThrow()
+  const beforeIssuedAt = ISSUED_AT.minus({ minute: 1 })
+  expect(() => Cacao.verify(cacao, { atTime: beforeIssuedAt.toJSDate() })).not.toThrow()
 })
 
 test('ok after exp if disableTimecheck option', async () => {
   const msg = new SiweMessage({
     ...SIWE_MESSAGE_PARAMS,
-    expirationTime: new Date(ISSUED_AT.valueOf() + 5 * 1000).toISOString(),
+    expirationTime: EXPIRATION_TIME.toISO(),
   })
 
   msg.signature = await ETHEREUM_WALLET.signMessage(msg.toMessage())
